4024ee251d2f2b886800323b27db1a4730da413a
Анастасия <eugengamilov@gmail.com>
2020-06-01 17:21:06 +0300

    Updates

---
+++ precache-manifest.ef425d0a3e4a811a2a539d7bd4f1a831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873b3b0d02e9012da68264b2392e695e",
+    "url": "/goit-react-hw-01-components/build/index.html"
+  },
+  {
+    "revision": "54a11ee3c5d1f7fed9b0",
+    "url": "/goit-react-hw-01-components/build/static/css/main.8f4c98be.chunk.css"
+  },
+  {
+    "revision": "053b2d1bb53390d8af1c",
+    "url": "/goit-react-hw-01-components/build/static/js/2.59591228.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/build/static/js/2.59591228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a11ee3c5d1f7fed9b0",
+    "url": "/goit-react-hw-01-components/build/static/js/main.00fd261a.chunk.js"
+  },
+  {
+    "revision": "d4b20b32d44062259565",
+    "url": "/goit-react-hw-01-components/build/static/js/runtime-main.bbb2fd2e.js"
+  }
+]);
